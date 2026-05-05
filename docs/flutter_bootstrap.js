3879bfb4f59d88c4517bd53e76d0f3bc49b5f363
shafienausri@gmail.com <shafienausri@gmail.com>
2026-05-05 17:15:48 +0800

    update app

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "316829381" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4095893294" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
